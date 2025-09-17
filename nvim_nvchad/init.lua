159a297cbc12c29e20f3af21528170b79460a78d
Jose Luis Ojosnegros Manchón <jl.ojosnegros.manchon@gmail.com>
2025-09-17 17:39:12 +0200

    nvim: Configure python3 provider
    Signed-off-by: Jose Luis Ojosnegros Manchón <jl.ojosnegros.manchon@gmail.com>

--- nvim_nvchad/init.lua
+++ nvim_nvchad/init.lua
@@ -1,6 +1,19 @@
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
 vim.g.mapleader = " "
 
+-- === Python 3 provider: habilitarlo y fijar intérprete ===
+-- Use el Python del venv para el provider de Neovim
+-- Be sure to install thid before:
+-- # 1) Asegura Python/pip
+-- sudo dnf install -y python3 python3-pip
+--
+-- # 2)  crea un venv dedicado para Neovim
+-- python3 -m venv ~/.venvs/neovim
+-- ~/.venvs/neovim/bin/python -m pip install --upgrade pip
+-- ~/.venvs/neovim/bin/pip install pynvim
+vim.g.loaded_python3_provider = nil  -- anula cualquier "0" que haya puesto NvChad/core
+vim.g.python3_host_prog = vim.fn.expand("~/.venvs/neovim/bin/python")
+
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
@@ -34,4 +47,10 @@ require "autocmds"
 
 vim.schedule(function()
   require "mappings"
+end)
+
+vim.schedule(function()
+  if vim.g.loaded_python3_provider == 0 then
+    vim.g.loaded_python3_provider = nil
+  end
 end)
