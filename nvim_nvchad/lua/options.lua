88f0dad276f472fb43a63a6c45bf74d6b9a65dd4
Jose Luis Ojosnegros Manchón <jl.ojosnegros.manchon@gmail.com>
2025-09-18 15:45:14 +0200

    nvim: some minor options
    Signed-off-by: Jose Luis Ojosnegros Manchón <jl.ojosnegros.manchon@gmail.com>

--- nvim_nvchad/lua/options.lua
+++ nvim_nvchad/lua/options.lua
@@ -4,3 +4,18 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+--
+--
+--
+local opt = vim.opt
+
+-- Search
+opt.incsearch = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.hlsearch = true
+
+-- Appearence
+opt.colorcolumn = "100"
+opt.signcolumn = "yes"
+-- opt.scrolloff = 30
